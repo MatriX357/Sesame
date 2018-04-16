1b969ca75949ba8a0f308f7cd9fc390e157a2d9a
mbrodziak <matousb96@gmail.com>
2018-04-16 16:34:29 +0200

    tenth commit

--- app/src/main/java/com/mxmbro/sesame/AddActivity.java
+++ app/src/main/java/com/mxmbro/sesame/AddActivity.java
@@ -1,21 +1,75 @@
 package com.mxmbro.sesame;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
-public class AddActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class AddActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnDatePicker;
+    EditText txtDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        btnDatePicker= findViewById(R.id.btn_date);
+        txtDate= findViewById(R.id.in_date);
+
+        btnDatePicker.setOnClickListener(this);
     }
 
+    public void onClick(View v) {
+
+        if (v == btnDatePicker) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            int mYear = c.get(Calendar.YEAR);
+            int mMonth = c.get(Calendar.MONTH);
+            int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @SuppressLint("SetTextI18n")
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @SuppressLint("SetTextI18n")
     public void confirm(View view) {
-        Intent intent = new Intent(getApplicationContext(), SesameActivity.class );
-        finish();
-        startActivity(intent);
+        TableLayout tl = findViewById(R.id.main_table);
+        TableRow row = new TableRow(this);
+        TextView tv = new TextView(this);
+        tv.setText("This is text");
+
+        tl.addView(row);
+        row.addView(tv);
     }
 }
+
--- app/src/main/java/com/mxmbro/sesame/SesameActivity.java
+++ app/src/main/java/com/mxmbro/sesame/SesameActivity.java
@@ -3,16 +3,13 @@ package com.mxmbro.sesame;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class SesameActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -21,15 +18,6 @@ public class SesameActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sesame);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
